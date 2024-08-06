92e4f600487ab8699309df2fd4a74cc0c4aaadbf
Pallavi Pandey <pallavipandey181@gmail.com>
2024-08-06 12:40:12 +0530

    17.59

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react";
-import "./App.css";
+import Register from "./Register";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <main className="App">
       <Register />
