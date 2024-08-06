5aa612c77b26cfe15e28a914fb3b7bde47854ba3
Pallavi Pandey <pallavipandey181@gmail.com>
2024-08-06 23:03:42 +0530

    21:14

--- frontend/src/components/Editor.jsx
+++ frontend/src/components/Editor.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Editor = () => {
   return (
-    <div>Editor</div>
-  )
-}
+    <section>
+      <h1>Editors Page</h1>
+      <br />
+      <p>You must have been assigned an Editor role.</p>
+      <div className="flexGrow">
+        <Link to="/">Home</Link>
+      </div>
+    </section>
+  );
+};
 
-export default Editor
+export default Editor;
