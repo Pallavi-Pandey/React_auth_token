ff496c4d0589b2120073e4b24e14e4fceaa2b198
Pallavi Pandey <pallavipandey181@gmail.com>
2024-08-07 00:28:01 +0530

    lesson3 auth-routes

--- frontend/src/components/Admin.jsx
+++ frontend/src/components/Admin.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Admin = () => {
   return (
-    <div>Admin</div>
-  )
-}
+    <section>
+      <h1>Admins Page</h1>
+      <br />
+      <p>You must have been assigned an Admin role.</p>
+      <div className="flexGrow">
+        <Link to="/">Home</Link>
+      </div>
+    </section>
+  );
+};
 
-export default Admin
+export default Admin;
